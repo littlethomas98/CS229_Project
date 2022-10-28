f34076180ff0af8efb15a9fb2b182b7a3828b38c
Thom <littlethomas98@gmail.com>
2022-10-28 01:13:33 -0700

    Updates

--- ThomCode/LoadData.py
+++ ThomCode/LoadData.py
@@ -1,23 +1,24 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import os
 # import tensorflow as tf
 
 # Load SO2 Data
 def loadSO2Data():
-    SO2_datafiles = os.listdir('../SO2 Data')
+    SO2_datafiles = os.listdir('./SO2 Data')
     SO2data = dict()
     for filename in SO2_datafiles:
-        SO2data[f'df_{filename}'] = pd.read_csv(f'../SO2 Data/{filename}')
+        SO2data[f'df_{filename}'] = pd.read_csv(f'./SO2 Data/{filename}')
     SO2_Data = pd.concat(SO2data)
     return SO2_Data
 
 # Load EQ Data
 def loadEQData():
-    EQ_datafiles = os.listdir('../EQ Data')
+    EQ_datafiles = os.listdir('./EQ Data')
     EQdata = dict()
     for filename in EQ_datafiles:
-        EQdata[f'df_{filename}'] = pd.read_csv(f'../EQ Data/{filename}')
+        EQdata[f'df_{filename}'] = pd.read_csv(f'./EQ Data/{filename}')
     EQ_Data = pd.concat(EQdata)
     return EQ_Data
 
@@ -25,43 +26,49 @@ def loadEQData():
 def convertDateFormat(EQ_Data, SO2_Data):
     SO2_Dates = np.zeros(len(SO2_Data['Date']))
     for i, data in enumerate(SO2_Data['Date']):
-        SO2_Dates[i] = data[0:2] + data[3:5] + data[6:10]
+        SO2_Dates[i] = int(data[0:2])*30 + int(data[3:5]) + (int(data[6:10])-2000)*365
 
     EQ_Dates = np.zeros(len(EQ_Data['time']))
     for i, data in enumerate(EQ_Data['time']):
-        EQ_Dates[i] = data[5:7] + data[8:10] + data[0:4]
+        EQ_Dates[i] = int(data[5:7])*30 + int(data[8:10]) + (int(data[0:4])-2000)*365
 
     SO2_Data['Date'] = SO2_Dates
     EQ_Data['time'] = EQ_Dates
 
     return EQ_Data, SO2_Data
 
-#Pull only data corresponding to EQ events
-def pullOnlyEventDates(EQ_Data, SO2_Data):
-    minLengthDataSet = min(len(EQ_Data['time']), len(SO2_Data['Date']))
-    if len(EQ_Data['time']) < len(SO2_Data['Date']):
-        DataShort = EQ_Data['time']
-        DataLong = SO2_Data['Date']
-    else: 
-        DataShort = SO2_Data['time']
-        DataLong = EQ_Data['time']
-
-    for i in range(len(DataShort)):
-        
-
-        print("egg1")
-    return 
-
-def plotData(EQ_Data, SO2_Data):
+#Save output data
+def saveData(EQ_Data, SO2_Data):
     EQ_Data.to_csv('EQ_Data.csv')
     SO2_Data.to_csv('SO2_Data.csv')
+    return 
+
+#Plot SO2 concentrations and EQ magnitudes
+def plotData(EQ_Data, SO2_Data):
+    fig = plt.figure()
+    ax1 = fig.add_subplot()
+    ax1.set_xlabel('Days (Normalized to 2018)')
+    ax1.set_ylabel('Relative Magnitude')
+    ax1.set_title('Relationship Between SO2 Concentration and EQ Event')
+
+    #Scale relative magnitude of SO2 concentration and EQ magnitude 
+    #   Note that EQ scale is exponentially scaled, this is due to the natrual 
+    #   phenomenon of earthquake
+    SO2Scale = 0.0015
+    EQScale = -2
+    DayAdjustment = 80
+
+    plt.plot(SO2_Data['Date']-(np.min(EQ_Data['time'])+DayAdjustment), SO2_Data['Daily Max 1-hour SO2 Concentration']*SO2Scale, 'o')
+    plt.plot(EQ_Data['time']-(np.min(EQ_Data['time'])+DayAdjustment), EQ_Data['mag']**EQScale, 'o')
+    plt.xlim([-10, 1600])
+    plt.legend(['SO2 Conc.', 'EQ Mag.'])
     return
 
 def main():
     SO2_Data = loadSO2Data()
     EQ_Data = loadEQData()
     convertDateFormat(EQ_Data, SO2_Data)
-    # pullOnlyEventDates(EQ_Data, SO2_Data)
+    saveData(EQ_Data, SO2_Data)
     plotData(EQ_Data, SO2_Data)
     print("egg")
     return
