9be516fa9cbe900a48e317f8d2cd487a4864b94a
Thom <littlethomas98@gmail.com>
2022-10-27 21:51:39 -0700

    Initial Upload

---
+++ LoadData.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+import os
+# import tensorflow as tf
+
+# Load SO2 Data
+SO2_datafiles = os.listdir('SO2 Data')
+SO2data = dict()
+for filename in SO2_datafiles:
+    SO2data[f'df_{filename}'] = pd.read_csv(f'SO2 Data/{filename}')
+SO2_Data = pd.concat(SO2data)
+
+# Load EQ Data
+EQ_datafiles = os.listdir('EQ Data')
+EQdata = dict()
+for filename in EQ_datafiles:
+    EQdata[f'df_{filename}'] = pd.read_csv(f'EQ Data/{filename}')
+EQ_Data = pd.concat(EQdata)
+
+#Convert Dates to Same Format
+SO2_Dates = np.zeros(len(SO2_Data['Date']))
+for i, data in enumerate(SO2_Data['Date']):
+    SO2_Dates[i] = data[0:2] + data[3:5] + data[6:10]
+
+EQ_Dates = np.zeros(len(EQ_Data['time']))
+for i, data in enumerate(EQ_Data['time']):
+    EQ_Dates[i] = data[5:7] + data[8:10] + data[0:4]
+
+SO2_Data['Date'] = SO2_Dates
+EQ_Data['time'] = EQ_Dates
+print("egg1")
+
+
