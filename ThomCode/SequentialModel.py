77194eb97abe2313e775f3e04f321b28068d4d79
Thom <littlethomas98@gmail.com>
2022-11-18 14:04:33 -0800

    Bias-Var update

--- ThomCode/SequentialModel.py
+++ ThomCode/SequentialModel.py
@@ -9,20 +9,25 @@ import os
 
 os.chdir('../CS229_Project/ThomCode')
 
-def main():
+def main(BiasVarStudy = False):
     """
     Function: createModel
         This function creates a Sequential TensorFlow model
 
-    Parameters: none
+    Parameters: BiasVarStudy - boolean value for whether to run a bias-variance study or
+                               whether to run typical model training
 
     Returns:    model - an untrained sequential TensorFlow model
     """   
     data = importData()
     mag_train, so2_train, mag_valid, so2_valid, mag_test, so2_test = splitData(data)
-    biasVar(mag_train, so2_train, mag_valid, so2_valid, mag_test, so2_test)
-    # model, _ = trainModel(mag_train, so2_train, mag_valid, so2_valid)
-    # testModel(model, mag_test, so2_test)
+
+    #Conduct Bias Variance Study?
+    if BiasVarStudy:
+        biasVar(mag_train, so2_train, mag_valid, so2_valid, mag_test, so2_test)
+    else:
+        model, _ = trainModel(mag_train, so2_train, mag_valid, so2_valid)
+        testModel(model, mag_test, so2_test)
     return
 
 def importData():
@@ -129,7 +134,7 @@ def trainModel(x_train, y_train, x_valid, y_valid):
 
     finalLoss = [history.history['loss'][-1], history.history['val_loss'][-1]]
 
-    # #Plot train vs validation error
+    #Plot train vs validation error
     # trainLoss = history.history['loss']
     # validLoss = history.history['val_loss']
     # plt.plot(trainLoss, label='Train')
@@ -143,7 +148,7 @@ def trainModel(x_train, y_train, x_valid, y_valid):
     return model, finalLoss
 
 
-def testModel(model, x_test, y_test):
+def testModel(model, x_test, y_test, BV=False):
     """
     Function: testModel
         This function trains the Sequential TensorFlow model on the provided training set
@@ -161,23 +166,24 @@ def testModel(model, x_test, y_test):
     print("Generate predictions")
     predictions = model.predict(x_test)
 
-    # #Plot 3D data
-    # fig = plt.figure()
-    # ax = fig.add_subplot(projection = '3d')
-    # ax.scatter(x_test[:,0], x_test[:,1], y_test, label = 'Real Data')
-    # ax.scatter(x_test[:,0], x_test[:,1], predictions)
-    # ax.set_xlabel('EQ Magnitude')
-    # ax.set_ylabel('Distance (km)')
-    # ax.set_zlabel('SO2 Concentration (ppm)')
+    if not BV:
+        #Plot 3D data
+        fig = plt.figure()
+        ax = fig.add_subplot(projection = '3d')
+        ax.scatter(x_test[:,0], x_test[:,1], y_test, label = 'Real Data')
+        ax.scatter(x_test[:,0], x_test[:,1], predictions)
+        ax.set_xlabel('EQ Magnitude')
+        ax.set_ylabel('Distance (km)')
+        ax.set_zlabel('SO2 Concentration (ppm)')
 
-    # #Determined that 3rd dimension is not relavent for current analysis, therefore reduce to 2d plot
-    # plt.figure()
-    # line1, line2 = plt.plot(x_test, y_test, 'bo', alpha = 0.2)
-    # line3, line4 = plt.plot(x_test, predictions, 'ro', alpha = 0.2, label = 'Predicted Data')
-    # plt.legend([line1, line3], ['Real Data', 'Predicted Data'], loc='best')
-    # plt.xlabel('Distance (km)')
-    # plt.ylabel('SO2 Concentration (ppm)')
-    # plt.savefig('Predictions.png')
+        #Determined that 3rd dimension is not relavent for current analysis, therefore reduce to 2d plot
+        plt.figure()
+        line1, line2 = plt.plot(x_test, y_test, 'bo', alpha = 0.2)
+        line3, line4 = plt.plot(x_test, predictions, 'ro', alpha = 0.2, label = 'Predicted Data')
+        plt.legend([line1, line3], ['Real Data', 'Predicted Data'], loc='best')
+        plt.xlabel('Distance (km)')
+        plt.ylabel('SO2 Concentration (ppm)')
+        plt.savefig('Predictions.png')
 
     #Plugin to visualize model
     # keras.utils.plot_model(
@@ -206,7 +212,7 @@ def biasVar(mag_train, so2_train, mag_valid, so2_valid, mag_test, so2_test):
         mag_test_sec = mag_test[0:int(i/10 * mag_test.shape[0]),:]
         so2_test_sec = so2_test[0:int(i/10 * so2_test.shape[0])]
         model, trainLoss = trainModel(mag_train_sec, so2_train_sec, mag_valid_sec, so2_valid_sec)
-        results = testModel(model, mag_test_sec, so2_test_sec)
+        results = testModel(model, mag_test_sec, so2_test_sec, BV=True)
         trainHist[i-1] = trainLoss
         testHist[i-1] = results
 
@@ -220,4 +226,4 @@ def biasVar(mag_train, so2_train, mag_valid, so2_valid, mag_test, so2_test):
 
     return
 
-main()
+main(BiasVarStudy=True)
